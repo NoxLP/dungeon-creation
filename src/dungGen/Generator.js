b9476b629f23f50c97f62a9e7d49bbfa5dc46591
Norber <nox_nox_@hotmail.com>
2022-05-29 10:51:22 +0100

    Single corridor without checking backwards for secondary ways

--- src/dungGen/Generator.js
+++ src/dungGen/Generator.js
@@ -27,6 +27,7 @@ export async function Generator(width, height, config, finishCallback) {
   const rooms = {};
   const emptyCells = {};
   const roomCells = {};
+  const corridors = [];
   const maxTries = config.maxTries || containerWidth * containerHeight * 0.25
   console.log('MAXTRIES: ', maxTries);
   const minSpaceBetweenRooms = !config
@@ -49,6 +50,9 @@ export async function Generator(width, height, config, finishCallback) {
   const coordsAreInsideMap = (coords) =>
     coords[0] > 0 && coords[0] < containerWidth
     && coords[1] > 0 && coords[1] < containerHeight
+  const coordsAreInsideMapIncluding0 = (coords) =>
+    coords[0] >= 0 && coords[0] < containerWidth
+    && coords[1] >= 0 && coords[1] < containerHeight
   const buildCellKey = (coords) => `${coords[0]},${coords[1]}`
   const getCoordsFromKey = (key) => {
     const match = key.match(/^(\d+),(\d+)/)
@@ -233,56 +237,143 @@ export async function Generator(width, height, config, finishCallback) {
         keys = Object.keys(emptyCells)
       }
     }
+
+    console.log('>>>>>>>>>> DONE GENERATING ROOMS')
   }
 
   const changeDirectionClockWise = (direction) => [-direction[1], direction[0]]
-  const getEmptyNearestCoords = (coords) => {
-    let direction = [1, 0]
-    const nearest = undefined
-    let tries = 0
 
-    while (!nearest && tries < maxTries) {
-      tries++
-
-    }
-  }
-  const canBeACorridor = (coords, horizontalCorridor) => {
+  const canBeACorridor = (coords, lastNode) => {
+    console.log('>>>>>>>> canBeACorridor');
     const minSpace = minSpaceBetweenCorridors + corridorsWidth
-    const getCheckCoords = horizontalCorridor
-      ? (i) => ([[coords[0], coords[1] - i], [coords[0], coords[1] + i]])
-      : (i) => ([[coords[0] - i, coords[1]], [coords[0] + i, coords[1]]])
+    const getCheckCoords = (i) => {
+      const c = []
+      let nextC = [coords[0] - 1, coords[1]] //W
+      const addCoords = () => {
+        if (!lastNode || (nextC[0] != lastNode[0] && nextC[1] != lastNode[1]))
+          c.push(nextC)
+      }
+      addCoords(nextC)
+      nextC = [coords[0] + 1, coords[1]] //E
+      addCoords(nextC)
+      nextC = [coords[0], coords[1] - 1] //N
+      addCoords(nextC)
+      nextC = [coords[0], coords[1] + 1] //S
+      addCoords(nextC)
+      nextC = [coords[0] - 1, coords[1] - 1] //NW
+      addCoords(nextC)
+      nextC = [coords[0] + 1, coords[1] - 1] //NE
+      addCoords(nextC)
+      nextC = [coords[0] - 1, coords[1] + 1] //SW
+      addCoords(nextC)
+      nextC = [coords[0] + 1, coords[1] + 1] //SE
+      addCoords(nextC)
+      return c
+    }
+    // ? (i) => ([[coords[0], coords[1] - i], [coords[0], coords[1] + i]])
+    // : (i) => ([[coords[0] - i, coords[1]], [coords[0] + i, coords[1]]])
 
     for (let i = 0; i < minSpace; i++) {
       const checkCoords = getCheckCoords(i)
+      console.log('CHECK COORDS ', i, checkCoords);
+      checkCoords.some((c) => {
+        console.log(c)
+        console.log(!emptyCells[buildCellKey(c)])
+        console.log((i != 0 ? !coordsAreInsideMapIncluding0(c)
+          : !coordsAreInsideMap(c)))
+      })
       if (checkCoords.some((c) => !emptyCells[buildCellKey(c)]
-        || !coordsAreInsideMap(c)))
+        || !coordsAreInsideMapIncluding0(c)))
         return false
     }
 
     return true
   }
   const generateCorridor = (start) => {
-    let stop = false
-    let currentCoords = start
     const nearNodes = [start]
-    let currentNode
-    const corridor = []
+    let currentNode, lastNode
+    const corridor = {}
+    let direction = [1, 0]
+    let tries = 0
 
-    while (nearNodes.length > 0) {
-      currentNode = nearNodes.shift()
-      if (canBeACorridor(currentNode)) {
-        corridor.push(currentNode)
-        nearNodes.push([currentNode[0] - 1, currentNode[1]]) //W
-        nearNodes.push([currentNode[0] + 1, currentNode[1]]) //E
-        nearNodes.push([currentNode[0], currentNode[1] - 1]) //N
-        nearNodes.push([currentNode[0], currentNode[1] + 1]) //S
+    const addNodeToCorridor = (coords) => {
+      const key = buildCellKey(coords)
+      corridor[key] = coords
+      delete emptyCells[key]
+      if (lastNode) {
+        direction = []
+        const compare = (first, second) => (
+          first == second
+            ? 0
+            : first > second
+              ? 1
+              : -1
+        )
+        direction[0] = compare(currentNode[0], lastNode[0])
+        direction[1] = compare(currentNode[1], lastNode[1])
       }
+    }
+    const addToNearNodes = (node) => {
+      if (emptyCells[buildCellKey(node)])
+        nearNodes.push(node)
+    }
+
+    while (nearNodes.length > 0 && tries < maxTries) {
+      tries++
+      lastNode = currentNode
+      currentNode = nearNodes.shift()
+      console.log('CURRNT NODE ', currentNode);
+      if (canBeACorridor(currentNode, lastNode)) {
+        console.log('canBeACorridor')
+        addNodeToCorridor(currentNode)
+        //if (direction) {
+        addToNearNodes([
+          currentNode[0] + direction[0],
+          currentNode[1] + direction[1]
+        ])
+        // } else {
+        //   addToNearNodes([currentNode[0] - 1, currentNode[1]]) //W
+        //   addToNearNodes([currentNode[0] + 1, currentNode[1]]) //E 
+        //   addToNearNodes([currentNode[0], currentNode[1] - 1]) //N
+        //   addToNearNodes([currentNode[0], currentNode[1] + 1]) //S}
+        // }
+        console.log('NEAR NODES ', nearNodes);
+      } else {
+        direction = changeDirectionClockWise(direction)
+        addToNearNodes([
+          currentNode[0] + direction[0],
+          currentNode[1] + direction[1]
+        ])
+        currentNode = lastNode
+      }
+      console.log('------------------------- DONE ', corridor);
     }
 
     return corridor
+    // const findCorridorStartingPosition = () => {
+
+    // }
   }
   const generateAllCorridors = () => {
+    let start = [1, 1]
+    let tries = 0
+    let currentCorridor = generateCorridor(start)
+    console.log('####### current corr ', currentCorridor);
+    if (Object.keys(currentCorridor).length > 0) {
+      corridors.push(currentCorridor)
+      console.log(corridors)
+    }
 
+
+    /* while (tries < maxTries) {
+      tries++
+      currentCorridor = generateCorridor(start)
+      //console.log(currentCorridor)
+      if (currentCorridor.length > 0) {
+        corridors.push(currentCorridor)
+        tries = 0
+      }
+    } */
   }
   // ******************
 
@@ -295,11 +386,13 @@ export async function Generator(width, height, config, finishCallback) {
   await generateRooms(
     config && config.maxRooms ? config.maxRooms : undefined
   )
+  generateAllCorridors()
 
   const result = {
     rooms: simpleGetProxy(rooms),
     emptyCells: simpleGetProxy(emptyCells),
-    roomCells: simpleGetProxy(roomCells)
+    roomCells: simpleGetProxy(roomCells),
+    corridors: simpleGetProxy(corridors)
   }
 
   if (finishCallback) finishCallback()
